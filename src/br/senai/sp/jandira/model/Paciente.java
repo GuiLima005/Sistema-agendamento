0913af16a9aea6e4cfbf2341067021a639764b07
GUILHERME DE OLIVEIRA LIMA <22282086@CFP127ALUNO.LOCAL>
2022-11-08 11:30:01 -0300

    Aula 08/11

--- src/br/senai/sp/jandira/model/Paciente.java
+++ src/br/senai/sp/jandira/model/Paciente.java
@@ -1,33 +1,11 @@
 package br.senai.sp.jandira.model;
 
-public class Paciente {
+public class Paciente extends Pessoa {
 
-    private String nome;
-    private String email;
-    private String cpf;
-    private String rg;
-    private String telefone;
-    private Genero genero;
     private String tipoSanguineo;
     private PlanoDeSaude planoDeSaude;
-    private Endereco endereco;
 
     //Métodos de acesso
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 
     public void setPlanoDeSaude(PlanoDeSaude planoDeSaude) {
         this.planoDeSaude = planoDeSaude;
@@ -37,52 +15,12 @@ public class Paciente {
         return planoDeSaude;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public Genero getGenero() {
-        return genero;
-    }
-
-    public void setGenero(Genero genero) {
-        this.genero = genero;
-    }
-
     public String getTipoSanguineo() {
         return tipoSanguineo;
     }
 
     public void setTipoSanguineo(String tipoSanguineo) {
         this.tipoSanguineo = tipoSanguineo;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
     }
 
 }
